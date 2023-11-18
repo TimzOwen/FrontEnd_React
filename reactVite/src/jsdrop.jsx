fa0ca77cff996780548d76c073e0544720ac350a
TimzOwen <timzowen@gmail.com>
2023-11-18 22:39:50 +0300

    children and props

--- reactVite/src/jsdrop.jsx
+++ reactVite/src/jsdrop.jsx
@@ -58,3 +58,84 @@ ReactDOM.render(
 // );
 
 
+
+
+// passing data using props
+function Button(props) {
+    // const handleClick =() => setCounter(counter+1);
+      return (
+      <button onClick={() => console.log("clicked")}>+5</button>
+    );
+  }
+  
+  
+  // Display counter
+  function Display(props){
+    return(
+    <div>{props.message}</div>  
+    );
+  }
+  
+  // function to render App
+  function App(){
+    const [counter, setCounter] = useState(250);
+    return (
+     <>
+      <Display message={counter}/>
+      <Button />
+      </>);
+  }
+  
+  
+  ReactDOM.render(
+   <App/>,
+    document.getElementById('mountNode'),
+  );
+  
+  // *** The React 18 way:
+  // root.render(
+  //   <Button />,
+  // );
+
+
+
+
+
+
+// Passing functions inside main App component to the children
+function Button(props) {
+	return (
+    <button onClick={props.updateCount}>+5</button>
+  );
+}
+
+
+// Display counter
+function Display(props){
+  return(
+  <div>{props.message}</div>  
+  );
+}
+
+// function to render App
+function App(){
+  const [counter, setCounter] = useState(250);
+  const updateCounter = () => setCounter(counter+5);
+  return (
+    <div>
+    <Button updateCount={updateCounter}/>
+    <Display message={counter}/>
+      </div>
+    );
+}
+
+
+ReactDOM.render(
+ <App/>,
+  document.getElementById('mountNode'),
+);
+
+// *** The React 18 way:
+// root.render(
+//   <Button />,
+// );

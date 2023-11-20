4c32e0e67761bb7c0ebb8593b305559e69a39eac
TimzOwen <timzowen@gmail.com>
2023-11-20 17:17:18 +0300

    card compnents

--- githubcards/githubcards/src/App.jsx
+++ githubcards/githubcards/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+class Card extends React.Component{
+  render(){
+    return (
+    <div className="github-profile">
+      <img src="https://placehold.it/75"/>
+      <div className="info">
+        <div className="name">name here...</div>
+        <div className="company">Company here...</div>
+        </div>
     </div>
-  )
+    );
+  }
 }
 
-export default App
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+      <div className="header">{this.props.title}</div>
+      <Card/>
+        </div>
+    )
+  }
+}
+
+// ReactDOM.render(
+// 	<App title="The GitHub Cards App." />,
+//   mountNode,
+// );
+
+// *** The React 18 way:
+// root.render(
+//   <App title="The GitHub Cards App" />,
+// );

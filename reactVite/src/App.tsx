56824c2923fb8f3da5f51b4391f84f18a9be0d25
TimzOwen <timzowen@gmail.com>
2023-11-24 20:47:09 +0300

    form set up

--- reactVite/src/App.tsx
+++ reactVite/src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import Button from "./components/Button/Button";
-import Like from "./components/Like/Like";
-import Message from "./Message";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import Form from "./components/Form";
 
 function App() {
-  const [cartItems, setCartItems] =useState(['product1', 'product2']);
-
-
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}/>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+      <Form />
     </div>
   );
 }

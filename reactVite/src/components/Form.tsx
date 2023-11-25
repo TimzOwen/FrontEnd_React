7c524509fd52e739c9e1f4f102987233aab69977
TimzOwen <timzowen@gmail.com>
2023-11-25 14:16:35 +0300

    form validation

--- reactVite/src/components/Form.tsx
+++ reactVite/src/components/Form.tsx
@@ -2,22 +2,37 @@ import React, { FormEvent, useRef, useState } from "react";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  interface FormData {
+    name: string;
+    age: number;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
-  
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>  
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">Name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">Name must have min of 3 characters</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
